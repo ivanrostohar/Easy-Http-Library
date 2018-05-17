2e724ba08ea741ee097a87dbca4706119b165a26
Ivan Rostohar <ivan.rostohar@nexgen.hr>
2018-05-17 12:14:51 +0200

    Added functions for POST, PUT and DELETE

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
 const oHttp = new easyHTTP();
 
 // Get Posts
-oHttp.get('https://jsonplaceholder.typicode.com/posts',
+// oHttp.get('https://jsonplaceholder.typicode.com/posts',
+//   function (error, response) {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log(response);
+//     }
+
+//   });
+
+const data = {
+  title: 'Custom post',
+  body: 'This is a custom post'
+};
+
+// Create Posts
+oHttp.post('https://jsonplaceholder.typicode.com/posts', data, function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(response);
+  }
+});
+
+// Change Posts
+oHttp.put('https://jsonplaceholder.typicode.com/posts/1', data, function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(response);
+  }
+});
+
+// Delete Post
+oHttp.delete('https://jsonplaceholder.typicode.com/posts/1',
   function (error, response) {
     if (error) {
       console.log(error);
     } else {
       console.log(response);
     }
-
   });
